06870095aab8dcc847268457b06c968a02f9f1e6
Lordmayur <148192595+Lordmayur@users.noreply.github.com>
2024-04-09 12:07:34 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,18 @@
+#functions in python
+def greet():
+    print("hello!")
+    print("good morning")
+greet()
+
+# add function
+def add(num1,num2):
+    sum= num1 + num2
+    print(sum)
+add(3,3)
+
+# addition and subtraction
+def add_sub(num1 , num2):
+    sum= num1 + num2
+    sub= num1 - num2
+    print(sum,sub)
+add_sub(4,8)
